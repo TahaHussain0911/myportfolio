71b102b42bedc18270ada42a470cb4dde75a8af9
TahaHussain <tahahussain0805@gmail.com>
2024-04-07 17:05:48 +0500

    project section c++ logo aligned | footer social media icons width changes on smaller devices

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-console.log("dbhawbd")
 const menu=document.querySelector('nav .middle');
 document.querySelector('.fa-bars').onclick=()=>{
     menu.classList.toggle('active');
-    // document.querySelector('.fa-bars').style.display='none';
-    // setTimeout(() => {
-    //     document.querySelector('.fa-xmark').style.display='inline-block';
-        
-    // }, 400);
-
 }
 window.onscroll=()=>{
     menu.classList.remove('active')
 }
 document.querySelector('.fa-xmark').onclick=()=>{
     menu.classList.remove('active');
-    // document.querySelector('.fa-xmark').style.display='none';
-    // setTimeout(() => {
-    //     document.querySelector('.fa-bars').style.display='inline-block';
-        
-    // }, 400);
-
 }
 let body=document.querySelector('body');
 let width=body.offsetWidth;
-console.log(width);
-// if(body.offsetWidth > 900){
-//     document.querySelector('.fa-xmark').style.display='none';
-//     document.querySelector('.fa-bars').style.display='none';
-// }
 const getAll=Array.from(document.querySelectorAll('input'));
 getAll.forEach((input)=>input.addEventListener('keydown',(event)=>{
     if (event.target.value.length===0 && event.keyCode===32) {
